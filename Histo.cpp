eaaf0f1ad972ade2ab6bb051e35faf814794dc94
usvoyager1 <32155479+usvoyager1@users.noreply.github.com>
2017-09-25 22:21:51 -0700

    Update Histo.cpp
    just add some formating

--- Histo.cpp
+++ Histo.cpp
@@ -15,13 +15,13 @@ vector<float> Histo::get_histo(void)
 	int results[10]={0,0,0,0,0,0,0,0,0,0};
 	vector<float> percents;
 	for(int a : this->buckets){
-		for(int i=0;i<10;i++){
-			if(a>=i*10 && a<=i*10+9)
+		for(int i=0; i<10; i++){
+			if( a >= i*10 && a <= i*10+9 )
 				results[i]++;
 		}
 	}
-	for(int i=0;i<10;i++){
-		percents.push_back(round((float)results[i]/this->buckets.size()*100)/100);
+	for(int i=0; i<10; i++){
+		percents.push_back(round( (float)results[i] / this->buckets.size() * 100) / 100);
 	}
 	return percents;
 }
